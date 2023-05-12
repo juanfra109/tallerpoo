85b948726465c923c5ea185b0396868f1395a846
juanfra109 <114134747+juanfra109@users.noreply.github.com>
2023-05-12 03:44:33 -0600

    algunos controladores agregados

--- TallerPOO/TallerPOO/Program.cs
+++ TallerPOO/TallerPOO/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TallerPOO.Models.dbModels;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ProyectofarmContext>(options =>
+     options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
